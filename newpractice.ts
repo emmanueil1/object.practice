8432e9ac49d312f2593b838464fe330e9a7ccb22
emmanueil1 <emmanueiljames@gmail.com>
2023-09-23 17:10:04 +0500

    practice

---
+++ newpractice.ts
@@ -0,0 +1,18 @@
+// var num1:number=5;
+// var num2:number=10;
+// var result:number=(num1+num2);
+// console.log (result);
+// var array1:number []=[1,2,3,4,5];
+// var array2:string[]=["ali", "mani","zahid","ghani"];
+// var array3=[{name:"adnan", age:40, job:"Rescue1122"},{name:"James", age:38, job:"Rescue1122"}];
+// for (let i = 0; i < array.length; i++)
+// {
+//     const element = array i];
+    
+// }
+// var array2:string[]=["ali", "mani","zahid","ghani"];
+// array2.pop();
+// console.log (array2)
+var array2:string[]=["ali", "mani","zahid","ghani"];
+var array3:string[]=array2.slice(1,2);
+console.log (array3);
