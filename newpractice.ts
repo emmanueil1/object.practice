067ebdf2e74609096cc9ed15df61cb17a245e63c
emmanueil1 <emmanueiljames@gmail.com>
2023-09-23 17:47:11 +0500

    2nd

--- newpractice.ts
+++ newpractice.ts
@@ -4,7 +4,7 @@
 // console.log (result);
 // var array1:number []=[1,2,3,4,5];
 // var array2:string[]=["ali", "mani","zahid","ghani"];
-// var array3=[{name:"adnan", age:40, job:"Rescue1122"},{name:"James", age:38, job:"Rescue1122"}];
+//var array3=[{name:"adnan", age:40, job:"Rescue1122"},{name:"James", age:38, job:"Rescue1122"}];
 // for (let i = 0; i < array.length; i++)
 // {
 //     const element = array i];
@@ -13,6 +13,25 @@
 // var array2:string[]=["ali", "mani","zahid","ghani"];
 // array2.pop();
 // console.log (array2)
-var array2:string[]=["ali", "mani","zahid","ghani"];
-var array3:string[]=array2.slice(1,2);
-console.log (array3);
+// var array2:string[]=["ali", "mani","zahid","ghani"];
+// var array3:string[]=array2.slice(1,2);
+// console.log (array3);
+
+
+
+
+
+
+let user =
+{
+    fName: "Emmanueil",
+    lName: "James",
+    age: 38,
+    hobbies:["sleeping", "resting", "let chill", "again sleeping"],
+    salary:()=> 5000,
+}
+// console.log (user.salary())
+// console.log (user.hobbies)
+
+delete user.salary
+console.log (user);
